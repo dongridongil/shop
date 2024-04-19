7ab2ddbcfcb6e9d25461d25174f733425c54119f
김동일 <ehddlfwkd32@naver.com>
2024-04-19 22:53:18 +0900

    [FIXED] 쇼핑몰앱 개발 ING

---
+++ src/data.js
@@ -0,0 +1,26 @@
+const data = [
+    {
+        id: 0,
+        title: "White and Black",
+        content: "Born in France",
+        // img: URL("https://codingapple1.github.io/shop/shoes1.jpg"),
+        price: 120000
+    },
+
+    {
+        id: 1,
+        title: "Red Knit",
+        content: "Born in Seoul",
+        // img: URL("https://codingapple1.github.io/shop/shoes2.jpg"),
+        price: 110000
+    },
+
+    {
+        id: 2,
+        title: "Grey Yordan",
+        content: "Born in the States",
+        // img: URL("https://codingapple1.github.io/shop/shoes3.jpg"),
+        price: 130000
+    }
+]
+export default data;
